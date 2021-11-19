6eff4a345ac18c4576514aea8a17057c4ee3ab8c
Matthias Clein <m.cleinespinoza@gmail.com>
2021-11-19 11:35:42 -0300

    cápsula 1: estructura básica

---
+++ estructura-basica/estructura-basica.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // Código para configurar la placa.
+  // Se ejecuta una sola vez, luego de que la placa se energiza.
+
+}
+
+void loop() {
+  // Código con las acciones principales. 
+  // Se ejecuta infinitamente hasta que la placa se desenergice. 
+}
